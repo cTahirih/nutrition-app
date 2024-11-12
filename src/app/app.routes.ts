c158843d8c82867377e654971e2fc7726a51746f
cjalirip <Cintia.Jaliri.-NDdisney.com>
2024-11-12 12:02:23 -0500

    initial config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '',
+  },
+  {
+    path: '',
+    loadChildren: () => import('./features/landing/landing.routes').then((m) => m.landingRoutes)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
